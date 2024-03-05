a3d6271721188ce7ab1ea0bc03e5d7f638c365d3
SofiiaKushai <sofiiakushai@gmail.com>
2024-03-05 13:08:41 +0200

    commit

---
+++ Homework_9.1/Homework_9.1.py
@@ -0,0 +1,5 @@
+# Create a file called hello.py that contains a single function hello().
+# This function should accept a single string parameter name print the text
+# Hello {name}! to the interactive window with {name} replaced with the
+# function argument. Add a file called main.py that imports the hello()
+# function from hello.py and calls the function with your name.
